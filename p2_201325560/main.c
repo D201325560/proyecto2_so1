f9925cf32d45f6193108834145ceecef94ad088e
D201325560 <d.daniel19@hotmail.com>
2018-01-08 01:32:18 -0600

    movimiento naves
    ya se mueven 3/4 filas de las naves solitas

--- p2_201325560/main.c
+++ p2_201325560/main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include<ncurses.h>
 #include<unistd.h>
+#include<string.h>
 #include<sys/sem.h>
 #include<sys/types.h>
 #include<sys/ipc.h>
 #include<stdlib.h>
+#include <time.h>
+#include <sys/shm.h>
+#define MAXSIZE     3
 
+int minutes=0, seconds=0, millisec=0;
 //#if defined(__GNU_LIBRARY__) && !deined()
+bool p1_puede_entrar,p2_puede_entrar;
 #define DELAY 30000
 //#if defined(__GNU_LIBRARY__) && !deined(__)
 //#else
@@ -16,98 +22,20 @@ union semun{
     unsigned short int * array;
     struct seminfo *__buf;
 };
+void die(char *s)
+{
+    perror(s);
+    exit(1);
+}
 //#endif
-/*int main(void){
-    key_t Llave;
-    int semaforo;
-    struct sembuf Proceso;
-    int i = 0 ;
-    Llave = ftok ("/bin/ls", 313);
-    semaforo = semget (Llave, 10, 0600 | IPC_CREAT);
-    semctl (semaforo, 0, SETVAL, 0);
-    Proceso.sem_num = 0;
-    Proceso.sem_op = -1;
-    Proceso.sem_flg = 0;
-    while (1){
-        if(i==11){
-            printf("PROGRAMA TERMINADO \n");
-            break;
-        }else{
-            printf("%d Esperando semaforo en verde.... \n",i);
-            semop (semaforo, &Proceso, 1);
-            // AREA PROTEGIDA
-            printf("%d Saliendo del area critica - Semaforo en rojo \n",i);
-            i++;
-        }
-    }
-}*/
-/*bool p1_puede_entrar=false;
-bool p2_puede_entrar=false;
-int turno=1;
-
-void proceso1(){
-    while(true){
-        p1_puede_entrar=true;
-        printf("hace algo 1");
-        while(p2_puede_entrar){
-            if(turno==2){
-                p1_puede_entrar=false;
-                while(turno==2){
-                }
-                p1_puede_entrar=true;
-            }
-        }
-        //region critica
-        printf("region critica1");
-        turno=2;
-        p1_puede_entrar=false;
-        printf("hace algo mas 1");
-    }
-}
-
-void proceso2(){
-    while(true){
-        printf("hace cosas2");
-        p2_puede_entrar=true;
-        while(p1_puede_entrar){
-            if(turno==1){
-                p2_puede_entrar=false;
-                while(turno==1){
-                }
-                p2_puede_entrar=true;
-            }
-        }
-        //region critica
-        printf("region critica2");
-        turno=1;
-        p2_puede_entrar=false;
-        printf("hace mas cosas2");
-    }
-}
-
-int main(void){
-    p1_puede_entrar=false;
-    p2_puede_entrar=false,
-    turno=1;
-    proceso1();
-    proceso2();
-}
-*/
 void down(int semid){
     struct sembuf Proceso;
-    int valor=0;
+    int valor=10;
     valor=semctl(semid,0,GETVAL);
-    if(valor>=0){
         Proceso.sem_num = 0;
         Proceso.sem_op = -1;
         Proceso.sem_flg = 0;
         semop(semid,&Proceso,1);
-    }else{
-        Proceso.sem_num = 0;
-        Proceso.sem_op = 1;
-        Proceso.sem_flg = 0;
-        semop(semid,&Proceso,1);
-    }
 }
 void up(int semid){
     struct sembuf Proceso;
@@ -122,30 +50,20 @@ void ventana_invasor(int max_y,int max_x){
     char msg2[]="espera mientras se conecta el defensor";
     erase();
     usleep(3);
+    //*******************semaforo para la pantalla
     key_t Llave;
     int semaforo;
-    //struct sembuf Proceso;
-    int i = 0 ;
+    int i=0;
     Llave = ftok ("/bin/ls", 33);
     semaforo = semget (Llave,1, 0600 | IPC_CREAT);
-    //semctl (semaforo, 0, SETVAL, 0);
-
-    //Proceso.sem_num = 0;
-    //Proceso.sem_op = 1;
-    //Proceso.sem_flg = 0;
-    /*for(i = 0 ; i < 11; i++) {
-        printf("%d Semaforo en verde \n",i);
-        //semop(semaforo,&Proceso, 1);
-        down(semaforo);
-        sleep(1);
-    }*/
+    semctl (semaforo, 0, SETVAL, 0);
+    //*******************fin semaforo para pantalla
     while (true){
-        if(i==11){
+        if(i==101){
             printf("PROGRAMA TERMINADO \n");
             sleep(10);
             break;
         }else{
-            //printf("%d Esperando semaforo en verde.... \n",i);
             initscr();
             mvprintw((max_y/2)-2,(max_x-strlen(msg))/2,"%s",msg);
             refresh();
@@ -153,12 +71,99 @@ void ventana_invasor(int max_y,int max_x){
             refresh();
             mvprintw((max_y/2)+2,(max_x-strlen(msg2))/2,"%s",msg2);
             refresh();
-            //semop (semaforo, &Proceso, 1);
-            down(semaforo);
-            // AREA PROTEGIDA
-            printf("%d Saliendo del area critica invasor - Semaforo en rojo \n",i);
-            sleep(10);
             up(semaforo);
+            // AREA PROTEGIDA PARA CRONOMETRO
+            erase();
+            int shmid;
+            key_t key;
+            char *shm, *s;
+            key = 5679;
+            //AREA PROTEGIDA PARA CRONOMETRO
+            int shmid1;
+            key_t key1;
+            char *shm1, *s1;
+            key1 = 6679;
+            int x=0,y=0;
+            //AREA PARA NAVES
+            while(true){
+                cont:
+                sleep(1);
+                millisec++;
+                if(millisec == 100)
+                {
+                    millisec = 0;
+                    seconds++;
+                    if(seconds == 60)
+                    {
+                        seconds = 0;
+                        minutes++;
+                    }
+                }
+                //inicia memoria compartida cliente*********************************
+                if ((shmid = shmget(key, MAXSIZE, 0666)) < 0)
+                    die("shmget");
+                if ((shm = shmat(shmid, NULL, 0)) == (char *) -1)
+                    die("shmat");
+                s =shm;
+                char total[10]="";
+                int cont=0;
+                char val[1];
+                val[0]=s[cont];
+                while (val[0] != 's'){
+                    int result=((char*)val)[0];
+                    if(result!=58){
+                        char integerString[3];
+                        sprintf(integerString,"%d",result);
+                        strcat(total,integerString);
+                    }else{
+                        strcat(total,val);
+                    }
+                    cont++;
+                    val[0]=s[cont];
+                }
+                clear();
+                mvprintw((max_y/2)-11,(max_x-strlen(msg))/2+34," %s ",total);
+                refresh();
+                if ((shmid1 = shmget(key1, MAXSIZE, IPC_CREAT | 0666)) < 0)
+                    die("shmget");
+                if ((shm1 = shmat(shmid1, NULL, 0)) == (char *) -1)
+                    die("shmat");
+                s1 = shm1;
+                char total1[40]="";
+                int cont1=0;
+                char val1[1];
+                val1[0]=
+                s1[cont1];
+                int inicio_fila=0;
+                while (val1[0] != 's'){
+                    inicio_fila++;
+                    int result1=((char*)val1)[0];
+                    if(inicio_fila==1){//aqui viene el valor de y
+                        y=result1;
+                    }else if(inicio_fila==2){//viene el valor de x
+                        x=result1;
+                    }else if(result1==7){//nave normal
+                        strcat(total1,"\\-.-/");
+                        strcat(total1,"  ");
+                    }else if(result1==8){//nave especial
+                        strcat(total1,"(/--\\)");
+                        strcat(total1,"  ");
+                    }else if(result1==9){
+                        inicio_fila=0;
+                        mvprintw(y,x," %s ",total1);
+                        //refresh();
+                        strcpy(total1,"");
+                    }
+                    cont1++;
+                    val1[0]=s1[cont1];
+                }
+                mvprintw((max_y/2)-11,(max_x-strlen(msg))/2+34," %s ",total);
+                refresh();
+                /*mvprintw(y,x," %s ",total1);
+                refresh();*/
+            }
+            //sleep(10);
+            down(semaforo);
             i++;
         }
     }
@@ -174,16 +179,13 @@ void ventana_defensor(int max_y,int max_x){
     key_t Llave;
     int semaforo;
     //struct sembuf Proceso;
-    int i = 0 ;
+    int i = 0;
     Llave = ftok ("/bin/ls", 33);
     semaforo = semget (Llave, 1, 0600 | IPC_CREAT);
-    //semctl (semaforo, 0, SETVAL, 0);
-    //Proceso.sem_num = 0;
-    //Proceso.sem_op = -1;
-    //Proceso.sem_flg = 0;
+    semctl (semaforo, 0, SETVAL, 0);
     usleep(3);
     while (true){
-        if(i==11){
+        if(i==10){
             printf("PROGRAMA TERMINADO \n");
             sleep(10);
             break;
@@ -198,9 +200,107 @@ void ventana_defensor(int max_y,int max_x){
             refresh();
             //semop (semaforo, &Proceso, 1);
             down(semaforo);
-            // AREA PROTEGIDA
-            printf("%d Saliendo del area critica defensor - Semaforo en rojo \n",i);
-            sleep(10);
+            // AREA PROTEGIDA PARA CRONOMETRO
+            erase();
+            int shmid;
+            key_t key;
+            char *shm, *s;
+            key = 5679;
+            //AREA PROTEGIDA PARA CRONOMETRO
+            int shmid1;
+            key_t key1;
+            char *shm1, *s1;
+            key1 = 6679;
+            //AREA PARA NAVES
+            int x=0,y=10;
+            int max_x1=0,max_y1=0;
+            int next_x=0;
+            int direction=6;
+            int total_fila=40;
+            while(true)
+            {
+                cont:
+                sleep(5);
+                millisec++;
+                if(millisec == 100)
+                {
+                    millisec = 0;
+                    seconds++;
+                    if(seconds == 60)
+                    {
+                        seconds = 0;
+                        minutes++;
+                    }
+                }
+                //inicio memoria compartida***********************
+                if ((shmid = shmget(key, MAXSIZE, IPC_CREAT | 0666)) < 0)
+                    die("shmget");
+                if ((shm = shmat(shmid, NULL, 0)) == (char *) -1)
+                    die("shmat");
+                s = shm;
+                *s = minutes;
+                s++;
+                *s = ':';
+                s++;
+                *s=seconds;
+                s++;
+                *s = ':';
+                s++;
+                *s = millisec;
+                s++;
+                *s = 's';
+                clear();
+                mvprintw((max_y/2)-11,(max_x-strlen(msg))/2+34,"  %d :  %d :  %d ", minutes, seconds, millisec);
+                refresh();
+                getmaxyx(stdscr,max_y1,max_x1);
+                if ((shmid1 = shmget(key1, MAXSIZE, IPC_CREAT | 0666)) < 0)
+                    die("shmget");
+                if ((shm1 = shmat(shmid1, NULL, 0)) == (char *) -1)
+                    die("shmat");
+                s1 = shm1;
+                mvprintw(y,x,"\\-.-/  \\-.-/  (/--\\)  \\-.-/  \\-.-/");
+                mvprintw(y+2,x+6,"\\-.-/  \\-.-/  (/--\\)  \\-.-/  \\-.-/");
+                mvprintw(y+4,x,"\\-.-/  \\-.-/  (/--\\)  \\-.-/  \\-.-/");
+                mvprintw(y+6,x+6,"\\-.-/  \\-.-/  (/--\\)  \\-.-/  \\-.-/");
+                refresh();
+                int k,l;
+                int aux_x,aux_y;
+                aux_x=x;
+                aux_y=y;
+                for(l=0;l<4;l++){
+                    *s1=y;
+                    s1++;
+                    *s1=x;
+                    s1++;
+                    for(k=0;k<5;k++){
+                        if(k==2){
+                            *s1=8;//para nave especial
+                            s1++;
+                        }else{
+                            *s1=7;//para nave especial
+                            s1++;
+                        }
+                    }
+                    *s1=9;//indica q finalizo la linea
+                    s1++;
+                    if(l==0||l==2){
+                     x=aux_x+6;
+                     //   x=0;
+                    }else{
+                        x=0;
+                    }
+                    y=y+2;
+                }
+                x=aux_x;
+                y=aux_y;
+                *s1='s';
+                next_x = x + direction;
+                if(next_x+total_fila >= max_x1 || next_x<0){
+                    direction*=-1;
+                }else{
+                    x+=direction;
+                }
+            }
             up(semaforo);
             i++;
         }
@@ -280,30 +380,5 @@ int main(void)
     }
     erase();
     ventana2(max_y,max_x);
-    return 0;
-    /*int x =0,y=0;
-    int max_x=0,max_y=0;
-    int next_x=0;
-    int direction=1;
-    char msg[]="Hola";
-    initscr();
-    noecho();
-    curs_set(FALSE);
-    while (1) {
-        getmaxyx(stdscr,max_y,max_x);
-        clear();
-        mvprintw(y,x,"-o-");
-        mvprintw(max_y-1,x,"-o-");
-        //mvprintw(max_y/2,(max_x-strlen(msg))/2,"%s",msg);
-        refresh();
-        usleep(DELAY);
-        next_x = x + direction;
-        if(next_x >= max_x || next_x<0){
-            direction*=-1;
-        }else{
-            x+=direction;
-        }
-    }
-    endwin();*/
-    //return 0;
+    return 0;    
 }
